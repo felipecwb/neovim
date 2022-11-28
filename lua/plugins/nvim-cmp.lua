6823b1fbc73a2b0783c78d94a18a526909f542f3
Felipe Francisco <felipefrancisco.cwb@gmail.com>
2022-11-28 10:50:50 -0300

    Fix the fixes

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,6 +5,26 @@ module.setup = function()
     local luasnip = require 'luasnip'
     local lspkind = require 'lspkind'
 
+    local next_item = function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+        else
+            fallback()
+        end
+    end
+
+    local previous_item = function(fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+        else
+            fallback()
+        end
+    end
+
     cmp.setup({
         sources = cmp.config.sources({
             { name = 'nvim_lsp' },
@@ -27,29 +47,15 @@ module.setup = function()
         completion = { completeopt = 'menu,menuone,noinsert' },
         mapping = {
             ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+            ['<Tab>'] = cmp.mapping(next_item, {"i", "s"}),
+            ['<S-Tab>'] = cmp.mapping(previous_item, {"i", "s"}),
+            ['<Down>'] = cmp.mapping(next_item, {"i", "s"}),
+            ['<Up>'] = cmp.mapping(previous_item, {"i", "s"}),
             ['<Esc>'] = cmp.mapping.close(),
             ['<CR>'] = cmp.mapping.confirm({
                 behavior = cmp.ConfirmBehavior.Insert,
                 select = true
             }),
-            ['<Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item()
-                elseif luasnip.expand_or_jumpable() then
-                    luasnip.expand_or_jump()
-                else
-                    fallback()
-                end
-            end, {"i", "s"}),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
-                else
-                    fallback()
-                end
-            end, {"i", "s"}),
         },
     })
 end
