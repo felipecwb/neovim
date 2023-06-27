6bd24151d9fe9abd29baa93af4ae0c958fbd3ad7
Felipe Francisco <felipefrancisco.cwb@gmail.com>
2023-06-27 20:40:02 -0300

    <html> <head><title>504 Gateway Time-out</title></head> <body> <center><h1>504 Gateway Time-out</h1></center> </body> </html>

--- lua/variables.lua
+++ lua/variables.lua
@@ -1,4 +1,3 @@
 vim.g.mapleader = ','
 
-vim.g.python_host_prog = vim.env.HOME .. '/.config/nvim/virtualenvs/py2/bin/python2'
 vim.g.python3_host_prog = vim.env.HOME .. '/.config/nvim/virtualenvs/py3/bin/python3'
