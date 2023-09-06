2122bf2b6ecb0ccf42f2cbdb4f29c294a70891e6
Felipe Francisco <felipefrancisco.cwb@gmail.com>
2023-09-06 01:46:13 -0300

    things occurred

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -7,8 +7,8 @@ function module.setup()
         sources = {
             null_ls.builtins.formatting.stylua,
             null_ls.builtins.formatting.prettier,
-            -- null_ls.builtins.formatting.ruff,
-            null_ls.builtins.formatting.isort,
+            -- null_ls.builtins.formatting.isort,
+            null_ls.builtins.formatting.ruff,
             null_ls.builtins.formatting.black,
             -- null_ls.builtins.diagnostics.eslint,
             null_ls.builtins.diagnostics.ruff,
