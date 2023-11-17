81ba5c1215e15494254813cfc8ae8ab76a84989f
Felipe Francisco <felipe@telnyx.com>
2023-11-17 18:25:03 -0300

    things occurred

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,7 +4,7 @@ return {
         config = function()
             vim.g.copilot_no_tab_map = true
             vim.g.copilot_assume_mapped = true
-            vim.g.copilot_tab_fallback = ""
+            -- vim.g.copilot_tab_fallback = ""
         end
     },
 }
